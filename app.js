d46a14b819b0906eb938aac9a871c57d8177f92a
namdery <-email--github@mail.com>
2026-09-13 03:07:13 +0300

    Set half-second dim tile animation

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (grid) {
   for (let index = 0; index < 810; index += 1) {
     const tile = document.createElement('i');
     tile.style.setProperty('--delay', `${-(index % 23) * 0.08}s`);
-    tile.style.setProperty('--duration', `${1.15 + (index % 7) * 0.18}s`);
+    tile.style.setProperty('--duration', '0.5s');
     grid.appendChild(tile);
   }
 }
